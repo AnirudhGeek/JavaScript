cffd2eefa3705208d139c97ddbcd5dc8e7c304d4
Anirudh Raturi <155432639+AnirudhGeek@users.noreply.github.com>
2024-12-13 16:13:50 +0000

    datatype summary

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -3,6 +3,13 @@
 // #1 PRIMITIVE : they all are call by value
 // 7types: String, Number, Boolean, null , undefined, Symbol, BigInt
 
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId);
+
+
+const bigNUmber = 23324746245455236n    // when we put n after a number it becomes a BigInt number 
+
 
 
 // Is javascript staically typed language or dynamically typed language 
@@ -13,3 +20,35 @@
 // #2 NON PRIMITIVE : reference type
 // Array, Objects , Functions
 
+const fruits = ["Apple","Mango","Banana"]   // eg of an array
+
+// objects are written in curly brackets
+let myObj = {
+    name: "Anirudh",
+    age: 22,
+}
+
+//functions(){}
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof bigNUmber);
+// REMINDER: learn type of from ECMAScrpit documentation
+console.log(typeof null);
+
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
