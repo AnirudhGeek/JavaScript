f27eefe8869c23ec4539520ae4f4422895e58138
Anirudh Raturi <155432639+AnirudhGeek@users.noreply.github.com>
2024-12-13 08:46:30 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer versions
+
+// alert(3+3)  // this will show an error because we are using node.js not browser
+console.log(3+3); console.log(5)  // it is not a good practice to read
+console.log(3+3)
+console.log(5)     // this below sintax is more readable 
+
+// MDN and tc39(Ecma script) both of these are good for reading the documentation
+
+
+let name = "Anirudh"
+let age = 18
+let isLoggedIn = false
+
+
+//Premitive data types 
+//number => 2 to power 53
+//bigint 
+//string ="" or ''
+//boolean => true/false
+//null=> standalone value, representation of an empty value 
+// undefined=> value has not assigned yet 
+//symbol => use cases while using react, use case is to find uniqueness 
+
+
+//object
+
+console.log(typeof name);
+console.log(typeof undefined);  // undefined 
+console.log(typeof null);  // object
+
